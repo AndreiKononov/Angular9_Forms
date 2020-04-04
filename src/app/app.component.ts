958abeb473ff1d8560ea93200ce94e52822b7107
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-04-05 01:25:52 +0300

    Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,72 +1,48 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-class Item {
-    purchase: string;
-    done: boolean;
-    price: number;
-
-    constructor(purchase: string, price: number) {
-        this.purchase = purchase;
-        this.price = price;
-        this.done = false;
-    }
+export class User {
+    name: string;
+    email: string;
+    phone: string;
 }
 
 @Component({
-    selector: 'purchase-app',
-    template: `<div class="page-header">
-        <h1> List of Purchases </h1>
-    </div>
-    <div class="panel">
-        <div class="form-inline">
+    selector: 'my-app',
+    template: `
+        <div>
             <div class="form-group">
-                <div class="col-md-8">
-                    <input class="form-control" [(ngModel)]="text" placeholder = "Title" />
+                <label>Name</label>
+                <input class="form-control" name="name" [(ngModel)]="user.name" #name="ngModel" required />
+                <div [hidden]="name.valid || name.untouched" class="alert alert-danger">
+                    Missing name
                 </div>
             </div>
             <div class="form-group">
-                <div class="col-md-6">
-                    <input type="number" class="form-control" [(ngModel)]="price" placeholder="Price" />
+                <label>Email</label>
+                <input class="form-control" name="email" [(ngModel)]="user.email" #email="ngModel" required pattern="[a-zA-Z_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}" />
+                <div [hidden]="email.valid || email.untouched" class="alert alert-danger">
+                    Incorrect email 
                 </div>
             </div>
             <div class="form-group">
-                <div class="col-md-offset-2 col-md-8">
-                    <button class="btn btn-default" (click)="addItem(text, price)">Add</button>
+                <label>Телефон</label>
+                <input class="form-control" name="phone" [(ngModel)]="user.phone" #phone="ngModel" required pattern="[0-9]{10}" />
+                <div [hidden]="phone.valid || phone.untouched" class="alert alert-danger">
+                    Incorrect phone number
                 </div>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-default" (click)="addUser()">Add</button>
             </div>
         </div>
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Purchased</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr *ngFor="let item of items">
-                <td>{{item.purchase}}</td>
-                <td>{{item.price}}</td>
-                <td><input type="checkbox" [(ngModel)]="item.done" /></td>
-            </tr>
-            </tbody>
-        </table>
-    </div>`
+    `
 })
 
 export class AppComponent {
-    text: string;
-    price: number = 0;
-    items: Item[] = [
-        { purchase: "Bread", done: false, price: 15.9 },
-        { purchase: "Oil", done: false, price: 60 },
-        { purchase: "Potatoes", done: true, price: 22.6 },
-        { purchase: "Cheese", done: false, price: 310 },
-    ];
 
-    addItem(text: string, price: number): void {
-        if (text == null || text.trim() == '' || price == null)
-            return;
-        this.items.push(new Item(text, price));
+    user: User = new User();
+
+    addUser() {
+        console.log(this.user);
     }
 }
