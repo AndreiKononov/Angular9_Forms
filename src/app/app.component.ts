c9c4f6a5ccfd4b7dda00ab4e0c5461a398467483
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-04-05 10:11:25 +0300

    Usage of relative form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,72 +1,55 @@
-import { Component } from "@angular/core";
-
-class Item {
-    purchase: string;
-    done: boolean;
-    price: number;
-
-    constructor(purchase: string, price: number) {
-        this.purchase = purchase;
-        this.price = price;
-        this.done = false;
-    }
-}
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-    selector: 'purchase-app',
-    template: `<div class="page-header">
-        <h1> List of Purchases </h1>
-    </div>
-    <div class="panel">
-        <div class="form-inline">
+    selector: 'my-app',
+    styles: [`
+        input.ng-touched.ng-invalid {border:solid red 2px;}
+        input.ng-touched.ng-valid {border:solid green 2px;}
+    `],
+    template: `
+        <form [formGroup]="myForm" novalidate (ngSubmit)="submit()">
             <div class="form-group">
-                <div class="col-md-8">
-                    <input class="form-control" [(ngModel)]="text" placeholder = "Title" />
+                <label>Name</label>
+                <input class="form-control" name="name" formControlName="userName" />
+                <div class="alert alert-danger" *ngIf="myForm.controls['userName'].invalid && myForm.controls['userName'].touched">
+                    Name missed
                 </div>
             </div>
             <div class="form-group">
-                <div class="col-md-6">
-                    <input type="number" class="form-control" [(ngModel)]="price" placeholder="Price" />
+                <label>Email</label>
+                <input class="form-control" name="email" formControlName="userEmail" />
+                <div class="alert alert-danger" *ngIf="myForm.controls['userEmail'].invalid && myForm.controls['userEmail'].touched">
+                    Incorrect email
                 </div>
             </div>
             <div class="form-group">
-                <div class="col-md-offset-2 col-md-8">
-                    <button class="btn btn-default" (click)="addItem(text, price)">Add</button>
+                <label>Телефон</label>
+                <input class="form-control" name="phone" formControlName="userPhone" />
+                <div class="alert alert-danger" *ngIf="myForm.controls['userPhone'].invalid && myForm.controls['userPhone'].touched">
+                    Incorrect phone number
                 </div>
             </div>
-        </div>
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Purchased</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr *ngFor="let item of items">
-                <td>{{item.purchase}}</td>
-                <td>{{item.price}}</td>
-                <td><input type="checkbox" [(ngModel)]="item.done" /></td>
-            </tr>
-            </tbody>
-        </table>
-    </div>`
+            <div class="form-group">
+                <button class="btn btn-default" [disabled]="myForm.invalid">Submit</button>
+            </div>
+        </form>
+    `
 })
 
 export class AppComponent {
-    text: string;
-    price: number = 0;
-    items: Item[] = [
-        { purchase: "Bread", done: false, price: 15.9 },
-        { purchase: "Oil", done: false, price: 60 },
-        { purchase: "Potatoes", done: true, price: 22.6 },
-        { purchase: "Cheese", done: false, price: 310 },
-    ];
 
-    addItem(text: string, price: number): void {
-        if (text == null || text.trim() == '' || price == null)
-            return;
-        this.items.push(new Item(text, price));
+    myForm : FormGroup;
+
+    constructor() {
+        this.myForm = new FormGroup({
+            "userName": new FormControl("Tom", Validators.required),
+            "userEmail": new FormControl("", [ Validators.required, Validators.email ]),
+            "userPhone": new FormControl("", Validators.pattern("[0-9]{10}")),
+        });
+    }
+
+    submit() {
+        console.log(this.myForm);
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
-import { AppComponent } from "./app.component";
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }   from './app.component';
+import { ReactiveFormsModule }   from '@angular/forms';
 
 @NgModule({
-    imports:        [ BrowserModule, FormsModule],
-    declarations:   [ AppComponent ],
-    bootstrap:      [ AppComponent ],
+    imports:      [ BrowserModule, FormsModule, ReactiveFormsModule],
+    declarations: [ AppComponent],
+    bootstrap:    [ AppComponent ],
 })
 
 export class AppModule { }
