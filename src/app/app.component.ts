6b32d4eaad09de30bd3d46b5bfc1618bf8628d18
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-04-05 03:27:11 +0300

    Submitting of form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,72 +1,49 @@
-import { Component } from "@angular/core";
-
-class Item {
-    purchase: string;
-    done: boolean;
-    price: number;
-
-    constructor(purchase: string, price: number) {
-        this.purchase = purchase;
-        this.price = price;
-        this.done = false;
-    }
-}
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-    selector: 'purchase-app',
-    template: `<div class="page-header">
-        <h1> List of Purchases </h1>
-    </div>
-    <div class="panel">
-        <div class="form-inline">
+    selector: 'my-app',
+    styles: [`
+        input.ng-touched.ng-invalid {border:solid red 2px;}
+        input.ng-touched.ng-valid {border:solid green 2px;}
+    `],
+    template: `
+        <form #myForm="ngForm" novalidate (ngSubmit)="onSubmit(myForm)">
+<!--        <form #myForm="ngForm" novalidate>-->
+<!--        <form class="ng-pristine ng-untouched ng-valid" novalidate="">-->
             <div class="form-group">
-                <div class="col-md-8">
-                    <input class="form-control" [(ngModel)]="text" placeholder = "Title" />
-                </div>
+                <label>Name</label>
+                <input class="form-control" name="name" [(ngModel)]="name" required />
             </div>
             <div class="form-group">
-                <div class="col-md-6">
-                    <input type="number" class="form-control" [(ngModel)]="price" placeholder="Price" />
-                </div>
+                <label>Email</label>
+                <input class="form-control" name="email" ngModel required email />
             </div>
             <div class="form-group">
-                <div class="col-md-offset-2 col-md-8">
-                    <button class="btn btn-default" (click)="addItem(text, price)">Add</button>
-                </div>
+                <label>Phone number</label>
+                <input class="form-control" name="phone" ngModel required pattern="[0-9]{11}" />
             </div>
-        </div>
-        <table class="table table-striped">
-            <thead>
-            <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Purchased</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr *ngFor="let item of items">
-                <td>{{item.purchase}}</td>
-                <td>{{item.price}}</td>
-                <td><input type="checkbox" [(ngModel)]="item.done" /></td>
-            </tr>
-            </tbody>
-        </table>
-    </div>`
+            <div class="form-group">
+<!--                <button [disabled]="myForm.invalid" class="btn btn-default" (click)="submit(myForm)">Add</button>-->
+                <input type="submit" [disabled]="myForm.invalid" class="btn btn-default" value="Submit" />
+            </div>
+        </form>
+        <div>Имя: {{myForm.value.name}}</div>
+        <div>Email: {{myForm.value.email}}</div>
+    `
 })
 
 export class AppComponent {
-    text: string;
-    price: number = 0;
-    items: Item[] = [
-        { purchase: "Bread", done: false, price: 15.9 },
-        { purchase: "Oil", done: false, price: 60 },
-        { purchase: "Potatoes", done: true, price: 22.6 },
-        { purchase: "Cheese", done: false, price: 310 },
-    ];
 
-    addItem(text: string, price: number): void {
-        if (text == null || text.trim() == '' || price == null)
-            return;
-        this.items.push(new Item(text, price));
+    name: string;
+    email: string;
+    phone: string;
+
+    // submit(form: NgForm) {
+    //     console.log(form);
+    // }
+
+    onSubmit(form: NgForm) {
+        console.log(form);
     }
 }
